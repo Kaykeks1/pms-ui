c128132337f917f8bd3f8375db658ae07e39a519
kayode <kayuskeks@gmail.com>
2022-12-13 01:33:27 +0100

    added sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link'
-import Layout from '../components/Layout'
+// import Layout from '../components/Layout'
+import MainLayout from '../components/MainLayout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  // <Layout title="Home | Next.js + TypeScript Example">
+  //   <h1>Hello Next.js 👋</h1>
+  //   <p>
+  //     <Link href="/about">About</Link>
+  //   </p>
+  // </Layout>
+  <MainLayout>
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
+     <p>
+       <Link href="/about">About</Link>
     </p>
-  </Layout>
+  </MainLayout>
 )
 
 export default IndexPage
