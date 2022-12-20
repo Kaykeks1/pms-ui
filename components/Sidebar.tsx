c7c13f120f899078978a7125fb751fbc74ee2592
kayode <kayuskeks@gmail.com>
2022-12-21 00:30:18 +0100

    added active link styles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 import profilePic from './gear.png'
 // import profilePic from './growth.png'
+import { useRouter } from "next/router";
 
-const Sidebar = () => (
+const Sidebar = () => {
+  const router = useRouter();
+  return (
   <div className="flex flex-col w-1/5 border-r border-gray-200 h-screen py-10">
     <div className="flex items-center mb-10">
         <Image
@@ -19,17 +22,26 @@ const Sidebar = () => (
     </div>
     <div className="flex flex-col h-screen justify-between">
         <div className="flex flex-col">
-            <Link className="flex py-1 my-2 h-12 ml-8 w-full group hover:ml-0 hover:before:p-1 hover:before:mr-6 hover:before:rounded-tr-lg hover:before:rounded-br-lg hover:before:bg-violet-600" href="/overview">
-                <FontAwesomeIcon icon={faThLarge} className="text-violet-400 group-hover:text-violet-600 self-center mr-3" />
-                <p className="text-gray-500 group-hover:text-violet-600 self-center">Overview</p>
+            <Link
+                className={`flex py-1 my-2 h-12 ml-8 w-full group hover:ml-0${router.pathname == "/overview" && ' ml-0 before:p-1 before:mr-6 before:rounded-tr-lg before:rounded-br-lg before:bg-violet-600'}`}
+                href="/overview"
+            >
+                <FontAwesomeIcon icon={faThLarge} className={`text-violet-400 self-center mr-3 ${router.pathname == "/overview" ? 'text-violet-600' : 'group-hover:text-violet-600'}`} />
+                <p className={`text-gray-500 self-center ${router.pathname == "/overview" ? 'text-violet-600' : 'group-hover:text-violet-600'}`}>Overview</p>
             </Link>
-            <Link className="flex py-1 my-2 h-12 ml-8 w-full group hover:ml-0 hover:before:p-1 hover:before:mr-6 hover:before:rounded-tr-lg hover:before:rounded-br-lg hover:before:bg-violet-600" href="/projects">
-                <FontAwesomeIcon icon={faTasks} className="text-violet-400 group-hover:text-violet-600 self-center mr-3" />
-                <p className="text-gray-500 group-hover:text-violet-600 self-center">Projects</p>
+            <Link
+                className={`flex py-1 my-2 h-12 ml-8 w-full group hover:ml-0${router.pathname == "/projects" && ' ml-0 before:p-1 before:mr-6 before:rounded-tr-lg before:rounded-br-lg before:bg-violet-600'}`}
+                href="/projects"
+            >
+                <FontAwesomeIcon icon={faTasks} className={`text-violet-400 self-center mr-3 ${router.pathname == "/projects" ? 'text-violet-600' : 'group-hover:text-violet-600'}`} />
+                <p className={`text-gray-500 self-center ${router.pathname == "/projects" ? 'text-violet-600' : 'group-hover:text-violet-600'}`}>Projects</p>
             </Link>
-            <Link className="flex py-1 my-2 h-12 ml-8 w-full group hover:ml-0 hover:before:p-1 hover:before:mr-6 hover:before:rounded-tr-lg hover:before:rounded-br-lg hover:before:bg-violet-600" href="/team">
-                <FontAwesomeIcon icon={faUsers} className="text-violet-400 group-hover:text-violet-600 self-center mr-3" />
-                <p className="text-gray-500 group-hover:text-violet-600 self-center">Teams</p>
+            <Link
+                className={`flex py-1 my-2 h-12 ml-8 w-full group hover:ml-0${router.pathname == "/team" && ' ml-0 before:p-1 before:mr-6 before:rounded-tr-lg before:rounded-br-lg before:bg-violet-600'}`}
+                href="/team"
+            >
+                <FontAwesomeIcon icon={faUsers} className={`text-violet-400 self-center mr-3 ${router.pathname == "/team" ? 'text-violet-600' : 'group-hover:text-violet-600'}`} />
+                <p className={`text-gray-500 self-center ${router.pathname == "/team" ? 'text-violet-600' : 'group-hover:text-violet-600'}`}>Teams</p>
             </Link>
         </div>
         <div className="flex flex-col">
@@ -41,5 +53,6 @@ const Sidebar = () => (
     </div>
   </div>
 )
+}
 
 export default Sidebar
