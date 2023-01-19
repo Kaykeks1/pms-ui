27d76eb07c5df1a2ec4504fd6ad23bf26192332d
kayode <kayuskeks@gmail.com>
2023-01-19 22:17:12 +0100

    added area chart design

--- pages/overview/index.tsx
+++ pages/overview/index.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/overview.module.css';
 import { faListUl, faTasks, faUsers } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState, useRef, useEffect } from 'react';
-import { LineChart, PieChart } from 'react-chartkick'
+import { AreaChart, PieChart } from 'react-chartkick'
 import 'chartkick/chart.js'
 import axios from 'axios';
 
@@ -34,15 +34,29 @@ const Overview = () => {
     { label: 'Number of tasks', slug: 'totalTasks', value: 0, icon: faListUl },
     { label: 'Number of members', slug: 'totalMembers', value: 0, icon: faUsers },
   ]
-  const [numbers, setNumbers] = useState(initialNumbers);
   const initialStatusChart = [["not_started", 44], ["started", 23], ["delayed", 44], ["completed", 23], ["on_hold", 44]]
+  const [numbers, setNumbers] = useState(initialNumbers);
   const [statusChart, setStatusChart] = useState(initialStatusChart);
+  const [gradientFill, setGradientFill] = useState<CanvasGradient>();
   const legend = ['not_started', 'started', 'delayed', 'completed', 'on_hold']
   const PieChartColors = ['#4B7BE5', '#A85CF9', '#5534A5', '#6FDFDF', '#F1EEF6']
 
   useEffect(() => {
     fetchOverview()
+    setGradientFill_()
   }, [])
+
+
+  const setGradientFill_ = () => {
+    if (!!document.getElementById('tasks-trend')) {
+      const canvas = document.getElementById('tasks-trend').firstElementChild as HTMLCanvasElement;
+      const ctx = canvas?.getContext("2d"); // canvas element
+      const gradientFill_ = ctx.createLinearGradient(0, 270, 0, -100);
+      gradientFill_.addColorStop(0, "#f5f5f5");
+      gradientFill_.addColorStop(1, "#7c3aed");
+      setGradientFill(gradientFill_);
+    }
+  }
 
   const fetchOverview = async () => {
     try {
@@ -59,7 +73,42 @@ const Overview = () => {
       console.log({ e })
     }
   }
+
+  const areachartDatasetOptions = {
+    // backgroundColor: 'rgb(2,0,36)',
+    backgroundColor: gradientFill,
+    pointRadius: 0,
+    borderColor: '#7c3aed',
+  }
+
+  const areachartLibraryOptions = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          drawOnChartArea: false,
+          drawBorder: false,
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+        border: {
+          display: false,
+        }
+      },
+      x: {
+        grid: {
+          drawBorder: false,
+          drawOnChartArea: false,
+          display: false,
+        },
+      },
+    },
+  }
   
+  const tasksDueWeeklyTrend = {"2021-01-01 00:00:00 -0800": 2, "2021-01-01 00:01:00 -0800": 5, "2021-01-01 00:02:00 -0800": 3, "2021-01-01 00:03:00 -0800": 7}
+
   return (
     <MainLayout title="Overview" pageTitle="Analytics">
       <div className={styles['analytics-overview']}>
@@ -97,7 +146,15 @@ const Overview = () => {
           </div>
         </div>
         <div className={styles['right-metrics']}>
-          <div className={styles['line-chart']}></div>
+          <div className={styles['line-chart']}>
+            <p>Tasks trend</p>
+            <AreaChart
+              id="tasks-trend"
+              data={tasksDueWeeklyTrend}
+              dataset={areachartDatasetOptions}
+              library={areachartLibraryOptions}
+            />
+          </div>
           <div className={styles['latest-list']}></div>
         </div>
       </div>
