7de5e4d7acc08c02be32810be823dadd11e2efb6
kayode <kayuskeks@gmail.com>
2023-01-26 23:38:15 +0100

    responsive auth

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -16,8 +16,9 @@ const AuthLayout = ({ children, title = 'This is the default title', pageTitle }
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <div className="auth flex flex-col bg-violet-600 h-screen">
-      <div className="flex w-full pl-16 pt-10 items-center">
+    <div className="auth flex flex-col bg-violet-600 h-screen bg-fixed">
+      {/* <div className="flex w-full pl-16 pt-10 items-center"> */}
+      <div className="absolute flex pl-16 pt-10 items-center">
         <Image
           className="object-cover w-12 h-12 ml-6 mr-4 rounded-full"
           src={profilePic}
@@ -27,9 +28,10 @@ const AuthLayout = ({ children, title = 'This is the default title', pageTitle }
         />
         <h1 className="text-3xl font-semi bold text-center text-white">PMS.</h1>
       </div>
-      <div className="flex w-full h-full">
-        <div className="flex w-1/2 text-7xl font-semi bold text-center text-white items-center justify-center h-full p-5">Project Management System.</div>
-        <div className="flex w-1/2 items-center justify-center h-full">
+      {/* md:h-full */}
+      <div className="flex flex-col md:flex-row w-full h-full justify-center pt-20 md:pt-0">
+        <div className="flex w-full md:w-1/2 text-4xl md:text-7xl font-semi bold text-center text-white md:items-center justify-center md:h-full p-5">Project Management System.</div>
+        <div className="flex w-full md:w-1/2 items-center justify-center md:h-full">
           <div className="flex flex-col bg-white w-3/5 p-10 rounded-lg">{children}</div>
         </div>
       </div>
