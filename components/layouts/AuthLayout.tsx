a280faab96fe9a4407ff2d8a8a1db625d9ac0ffd
kayode <kayuskeks@gmail.com>
2023-02-08 22:39:59 +0100

    remove comment in layout

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -17,7 +17,6 @@ const AuthLayout = ({ children, title = 'This is the default title', pageTitle }
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <div className="auth flex flex-col bg-violet-600 h-screen bg-fixed">
-      {/* <div className="flex w-full pl-16 pt-10 items-center"> */}
       <div className="absolute flex pl-16 pt-10 items-center">
         <Image
           className="object-cover w-12 h-12 ml-6 mr-4 rounded-full"
@@ -28,7 +27,6 @@ const AuthLayout = ({ children, title = 'This is the default title', pageTitle }
         />
         <h1 className="text-3xl font-semi bold text-center text-white">PMS.</h1>
       </div>
-      {/* md:h-full or h-full */}
       <div className="flex flex-col md:flex-row w-full md:h-full justify-center pt-60 pb-20 md:pt-0 md:pb-0 overflow-auto z-10">
         <div className="flex w-full md:w-1/2 text-4xl md:text-7xl font-semi bold text-center text-white md:items-center justify-center md:h-full p-5">Project Management System.</div>
         <div className="flex w-full md:w-1/2 items-center justify-center md:h-full">
